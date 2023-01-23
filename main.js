2836a47cb913c6398e429cfd99c2a90194eea929
Maksym Zhuk <max2001zh@gmail.com>
2023-01-23 16:02:06 +0200

    implement gif creation

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ let surface;                    // A surface model
 let shProgram;                  // A shader program
 let spaceball;                  // A SimpleRotator object that lets the user rotate the view by mouse.
 
+let handlePosition = 0.0;
+
+let a = 6;
+let b = 20;
+
 function deg2rad(angle) {
     return angle * Math.PI / 180;
 }
@@ -16,22 +21,22 @@ function Model(name) {
     this.iVertexBuffer = gl.createBuffer();
     this.count = 0;
 
-    this.BufferData = function(vertices) {
-
+    this.BufferData = function (vertices) {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
+        this.count = vertices.length / 3;
+    };
 
-        this.count = vertices.length/3;
-    }
-
-    this.Draw = function() {
-
+    this.Draw = function () {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribVertex);
-   
-        gl.drawArrays(gl.LINE_STRIP, 0, this.count);
-    }
+
+        gl.vertexAttribPointer(shProgram.iNormal, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iNormal);
+
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count);
+    };
 }
 
 
@@ -48,9 +53,21 @@ function ShaderProgram(name, program) {
     // Location of the uniform matrix representing the combined transformation.
     this.iModelViewProjectionMatrix = -1;
 
-    this.Use = function() {
+    this.iNormal = -1;
+    this.iNormalMatrix = -1;
+
+    this.iAmbientColor = -1;
+    this.iDiffuseColor = -1;
+    this.iSpecularColor = -1;
+
+    this.iShininess = -1;
+
+    this.iLightPosition = -1;
+    this.iLightVec = -1;
+
+    this.Use = function () {
         gl.useProgram(this.prog);
-    }
+    };
 }
 
 
@@ -58,54 +75,71 @@ function ShaderProgram(name, program) {
  * (Note that the use of the above drawPrimitive function is not an efficient
  * way to draw with WebGL.  Here, the geometry is so simple that it doesn't matter.)
  */
-function draw() { 
-    gl.clearColor(0,0,0,1);
+function draw() {
+    gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
+
     /* Set the values of the projection transformation */
-    let projection = m4.perspective(Math.PI/8, 1, 8, 12); 
-    
+    let projection = m4.perspective(Math.PI / 10, 1, 8, 1000);
+
     /* Get the view matrix from the SimpleRotator object.*/
     let modelView = spaceball.getViewMatrix();
 
-    let rotateToPointZero = m4.axisRotation([0.707,0.707,0], 0.7);
-    let translateToPointZero = m4.translation(0,0,-10);
+    let rotateToPointZero = m4.axisRotation([0.707, 0.707, 0], 0.7);
+    let translateToPointZero = m4.translation(0, 0, -10);
 
-    let matAccum0 = m4.multiply(rotateToPointZero, modelView );
-    let matAccum1 = m4.multiply(translateToPointZero, matAccum0 );
-        
+    let matAccum0 = m4.multiply(rotateToPointZero, modelView);
+    let matAccum1 = m4.multiply(translateToPointZero, matAccum0);
+    const modelViewInverse = m4.inverse(matAccum1, new Float32Array(16));
+    const normalMatrix = m4.transpose(modelViewInverse, new Float32Array(16));
+
     /* Multiply the projection matrix times the modelview matrix to give the
        combined transformation matrix, and send that to the shader program. */
-    let modelViewProjection = m4.multiply(projection, matAccum1 );
+    let modelViewProjection = m4.multiply(projection, matAccum1);
 
-    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection );
-    
+    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection);
+    gl.uniformMatrix4fv(shProgram.iNormalMatrix, false, normalMatrix);
+
+    gl.uniform3fv(shProgram.iLightPosition, lightCoordinates());
+    gl.uniform3fv(shProgram.iLightDirection, [1, 0, 0]);
+
+    gl.uniform3fv(shProgram.iLightVec, new Float32Array(3));
+
+    gl.uniform1f(shProgram.iShininess, 1.0);
+
+    gl.uniform3fv(shProgram.iAmbientColor, [0.5, 0, 0.4]);
+    gl.uniform3fv(shProgram.iDiffuseColor, [1.3, 1.0, 0.0]);
+    gl.uniform3fv(shProgram.iSpecularColor, [1.5, 1.0, 1.0]);
     /* Draw the six faces of a cube, with different colors. */
-    gl.uniform4fv(shProgram.iColor, [1,1,0,1] );
+    gl.uniform4fv(shProgram.iColor, [1, 1, 0, 1]);
 
     surface.Draw();
 }
 
-function CreateSurfaceData()
-{
-    let vertexList = [];
+function CreateSurfaceData() {
+    function CreateSurfaceData()
+    {
+        let vertexList = [];
 
-    const POINTS = 100;
+        const POINTS = 100;
 
-    for (let i = 0; i <= POINTS; i++) {
+        for (let i = 0; i <= POINTS; i++) {
 
-        const u = i * 2 * Math.PI / POINTS;
+            const u = i * 2 * Math.PI / POINTS;
 
-        for (let j = 0; j < POINTS; j++) {
+            for (let j = 0; j < POINTS; j++) {
 
-            const v = j * 2 * Math.PI / POINTS;
+                const v = j * 2 * Math.PI / POINTS;
 
-            const x = Math.pow(Math.cos(u) * Math.cos(v), 3);
-            const y = Math.pow(Math.sin(u) * Math.cos(v), 3);
-            const z = Math.pow(Math.sin(v), 3);
+                const x = Math.pow(Math.cos(u) * Math.cos(v), 3);
+                const y = Math.pow(Math.sin(u) * Math.cos(v), 3);
+                const z = Math.pow(Math.sin(v), 3);
 
-            vertexList.push(x, y, z);
+                vertexList.push(x, y, z);
+            }
         }
+
+        return vertexList;
     }
 
     return vertexList;
@@ -114,16 +148,28 @@ function CreateSurfaceData()
 
 /* Initialize the WebGL context. Called from init() */
 function initGL() {
-    let prog = createProgram( gl, vertexShaderSource, fragmentShaderSource );
+    let prog = createProgram(gl, vertexShaderSource, fragmentShaderSource);
 
-    shProgram = new ShaderProgram('Basic', prog);
+    shProgram = new ShaderProgram("Basic", prog);
     shProgram.Use();
 
-    shProgram.iAttribVertex              = gl.getAttribLocation(prog, "vertex");
+    shProgram.iAttribVertex = gl.getAttribLocation(prog, "vertex");
     shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
-    shProgram.iColor                     = gl.getUniformLocation(prog, "color");
+    shProgram.iColor = gl.getUniformLocation(prog, "color");
 
-    surface = new Model('Surface');
+    shProgram.iNormal = gl.getAttribLocation(prog, "normal");
+    shProgram.iNormalMatrix = gl.getUniformLocation(prog, "normalMatrix");
+
+    shProgram.iAmbientColor = gl.getUniformLocation(prog, "ambientColor");
+    shProgram.iDiffuseColor = gl.getUniformLocation(prog, "diffuseColor");
+    shProgram.iSpecularColor = gl.getUniformLocation(prog, "specularColor");
+
+    shProgram.iShininess = gl.getUniformLocation(prog, "shininess");
+
+    shProgram.iLightPosition = gl.getUniformLocation(prog, "lightPosition");
+    shProgram.iLightVec = gl.getUniformLocation(prog, "lightVec");
+
+    surface = new Model("Surface");
     surface.BufferData(CreateSurfaceData());
 
     gl.enable(gl.DEPTH_TEST);
@@ -139,24 +185,24 @@ function initGL() {
  * source code for the vertex shader and for the fragment shader.
  */
 function createProgram(gl, vShader, fShader) {
-    let vsh = gl.createShader( gl.VERTEX_SHADER );
-    gl.shaderSource(vsh,vShader);
+    let vsh = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vsh, vShader);
     gl.compileShader(vsh);
-    if ( ! gl.getShaderParameter(vsh, gl.COMPILE_STATUS) ) {
+    if (!gl.getShaderParameter(vsh, gl.COMPILE_STATUS)) {
         throw new Error("Error in vertex shader:  " + gl.getShaderInfoLog(vsh));
-     }
-    let fsh = gl.createShader( gl.FRAGMENT_SHADER );
+    }
+    let fsh = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fsh, fShader);
     gl.compileShader(fsh);
-    if ( ! gl.getShaderParameter(fsh, gl.COMPILE_STATUS) ) {
-       throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
+    if (!gl.getShaderParameter(fsh, gl.COMPILE_STATUS)) {
+        throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
     }
     let prog = gl.createProgram();
-    gl.attachShader(prog,vsh);
+    gl.attachShader(prog, vsh);
     gl.attachShader(prog, fsh);
     gl.linkProgram(prog);
-    if ( ! gl.getProgramParameter( prog, gl.LINK_STATUS) ) {
-       throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
+    if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
+        throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
     }
     return prog;
 }
@@ -170,21 +216,17 @@ function init() {
     try {
         canvas = document.getElementById("webglcanvas");
         gl = canvas.getContext("webgl");
-        if ( ! gl ) {
+        if (!gl) {
             throw "Browser does not support WebGL";
         }
-    }
-    catch (e) {
-        document.getElementById("canvas-holder").innerHTML =
-            "<p>Sorry, could not get a WebGL graphics context.</p>";
+    } catch (e) {
+        document.getElementById("canvas-holder").innerHTML = "<p>Sorry, could not get a WebGL graphics context.</p>";
         return;
     }
     try {
         initGL();  // initialize the WebGL graphics context
-    }
-    catch (e) {
-        document.getElementById("canvas-holder").innerHTML =
-            "<p>Sorry, could not initialize the WebGL graphics context: " + e + "</p>";
+    } catch (e) {
+        document.getElementById("canvas-holder").innerHTML = "<p>Sorry, could not initialize the WebGL graphics context: " + e + "</p>";
         return;
     }
 
@@ -192,3 +234,36 @@ function init() {
 
     draw();
 }
+
+window.addEventListener("keydown", function (event) {
+    switch (event.key) {
+        case "ArrowLeft":
+            left();
+            break;
+        case "ArrowRight":
+            right();
+            break;
+        default:
+            return;
+    }
+});
+
+const left = () => {
+    handlePosition -= 0.07;
+    reDraw();
+};
+
+const right = () => {
+    handlePosition += 0.07;
+    reDraw();
+};
+
+const lightCoordinates = () => {
+    let coord = Math.sin(handlePosition) * 1.2;
+    return [coord, -2, coord * coord];
+};
+
+const reDraw = () => {
+    surface.BufferData(CreateSurfaceData());
+    draw();
+};
